d3dde2729961b35717e25f6882e10815e59120c8
WangTingZheng <2251985371@qq.com>
2020-03-26 13:08:53 +0800

    fix(Set Java source):
    Set Java source to 1.8

--- src/main/java/com/wangtingzheng/smartconf/client/Client.java
+++ src/main/java/com/wangtingzheng/smartconf/client/Client.java
@@ -15,20 +15,16 @@ import java.util.Map;
 
 public class Client{
     public static void main(String[] args) throws IOException {
-        //客户端请求与本机在20006端口建立TCP连接
         Socket client = new Socket("127.0.0.1", 20005);
         client.setSoTimeout(10000);
-        //获取键盘输入
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        //获取Socket的输出流，用来发送数据到服务端
         PrintStream out = new PrintStream(client.getOutputStream());
-        //获取Socket的输入流，用来接收从服务端发送过来的数据
         BufferedReader buf = new BufferedReader(new InputStreamReader(client.getInputStream()));
         boolean flag = true;
 
         while (flag)
         {
-            System.out.println("开始发送？");
+            System.out.println("start?");
             String str = input.readLine();
             if (str.equals("yes")) {
                 Map<String, String> wifiMap = new HashMap<>();
@@ -51,7 +47,6 @@ public class Client{
                 String data = wifiList.listToString(false);
                 out.println(data);
                 try {
-                    //从服务器端接收数据有个时间限制（系统自设，也可以自己设置），超过了这个时间，便会抛出该异常
                     String echo = buf.readLine();
                     System.out.println(echo);
                 } catch (SocketTimeoutException e) {
@@ -65,8 +60,7 @@ public class Client{
         }
         input.close();
         if (client != null) {
-            //如果构造函数建立起了连接，则关闭套接字，如果没有建立起连接，自然不用关闭
-            client.close();    //只关闭socket，其关联的输入输出流也会被关闭
+            client.close();
         }
     }
 }
--- src/main/java/com/wangtingzheng/smartconf/thread/ServerThread.java
+++ src/main/java/com/wangtingzheng/smartconf/thread/ServerThread.java
@@ -10,7 +10,7 @@ import java.net.Socket;
 
 
 /**
- * 该类为多线程类，用于服务端
+ *
  */
 public class ServerThread implements Runnable {
 
@@ -22,13 +22,10 @@ public class ServerThread implements Runnable {
     @Override
     public void run() {
         try{
-            //获取Socket的输出流，用来向客户端发送数据
             PrintStream out = new PrintStream(client.getOutputStream());
-            //获取Socket的输入流，用来接收从客户端发送过来的数据
             BufferedReader buf = new BufferedReader(new InputStreamReader(client.getInputStream()));
             boolean flag =true;
             while(flag){
-                //接收从客户端发送过来的数据
                 String str =  buf.readLine();
                 if(str == null || "".equals(str)){
                     flag = false;
